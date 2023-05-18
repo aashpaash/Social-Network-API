af7f6b40d2ce014450dd66cedd0f0f66ffb61adf
aashpaash <115119317+aashpaash@users.noreply.github.com>
2023-05-17 23:49:45 -0700

    fixed errors in user controller file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,30 +2,27 @@
 const { User, Thought } = require('../models');
 
 const userController = {
-  // Get all users
-    getAllUsers(req, res) {
-        User.find({})
-            .populate({ path: "thoughts", select: "-__v" })
-            .populate({ path: "friends", select: "-__v" })
-            .select("-__v")
-            .then((dbUsersData) => res.json(dbUsersData))
-            .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-            });
-    },
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({ path: "thoughts", select: "-__v" })
+      .populate({ path: "friends", select: "-__v" })
+      .select("-__v")
+      .then((dbUsersData) => res.json(dbUsersData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
-    // Get user by ID
-    getUserId({req, res) {
-     User.findOne({ _id: req.params.userId })
+  // Get user by id
+  getUserId(req, res) {
+    User.findOne({ _id: req.params.userId })
       .populate('thoughts')
       .populate('friends')
       .select('-__v')
       .then((dbUserData) => {
         if (!dbUserData) {
-          return res
-          .status(404)
-          .json({ message: 'Unable to complete action' });
+          return res.status(404).json({ message: 'Unable to complete action' });
         }
         res.json(dbUserData);
       })
@@ -34,9 +31,9 @@ const userController = {
         res.status(500).json(err);
       });
   },
-    // Create user
-    createUser(req, res) {
-        User.create(req.body)
+ // Create a new user
+  createUser(req, res) {
+    User.create(req.body)
       .then((dbUserData) => {
         res.json(dbUserData);
       })
@@ -45,56 +42,70 @@ const userController = {
         res.status(500).json(err);
       });
   },
-
+ 
   // Update user
   updateUser(req, res) {
     User.findOneAndUpdate(
-        {_id: req.params.userId},
+      { _id: req.params.userId },
       req.body,
-      {new: true},
-      {runValidators: true},
-
-    )}
-    .then((dbUserData) => {
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
         if (!dbUserData) {
-            return res
-            .status(404)
-            .json({message: "Unable to complete action."});
-            return;
+          return res.status(404).json({ message: "Unable to complete action." });
         }
         res.json(dbUserData);
-    })
-    .catch((err) => res.json(err))
-  }
+      })
+      .catch((err) => res.json(err));
+  },
 
   // Delete user
   deleteUser(req, res) {
-    User.findOneAndDelete(
-        {_id: req.params.userId},
-        req.body,
-        {new: true},
-        {runValidators: true},)
-        .then((dbUserData) => {
-            if (!dbUserData) {
-                return res
-                .status(404)
-                .json({message: "Unable to complete action."});
-            }
-            return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
-        })
-        .then(() => {
-          res.json({ message: 'Successfully deleted!' });
-        })
-        .catch((err) => res.json(err));
-    }
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "Unable to complete action." });
+        }
+        return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+      })
+      .then(() => {
+        res.json({ message: 'Successfully deleted!' });
+      })
+      .catch((err) => res.json(err));
+  },
 
+  // Remove friend
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "Unable to complete action." });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 
+  // Add friend
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "Unable to complete action." });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  }
+};
 
-
-
-
-
-
-
-
+// Export module
 module.exports = userController;
